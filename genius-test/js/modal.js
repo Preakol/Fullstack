0192e730081f3af9baec7281a3c86b3995c5def3
Preakol <filonenko3001gosha@gmail.com>
2024-01-11 13:29:26 +0200

    Last changes for SHOP BAKERY project :D

--- genius-test/js/modal.js
+++ genius-test/js/modal.js
@@ -1,8 +1,10 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnOpen2 = document.querySelector('.modal-btn-open2');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
 
 modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnOpen2.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
