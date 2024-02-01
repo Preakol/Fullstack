c663f1bdbd664f1e33842ffe1840877af807d1b0
Preakol <filonenko3001gosha@gmail.com>
2024-02-01 13:10:00 +0200

    Updated mobile menu and modal js codes

--- genius-test/js/modal.js
+++ genius-test/js/modal.js
@@ -1,10 +1,12 @@
 const modal = document.querySelector('.backdrop');
-const modalBtnOpen = document.querySelector('.modal-btn-open');
-const modalBtnOpen2 = document.querySelector('.modal-btn-open2');
+const modalBtnsOpen = document.querySelectorAll('.modal-btn-open')
+// const modalBtnOpen2 = document.querySelector('.modal-btn-open2');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const toggleModal = () => modal.classList.toggle('is-hidden');
+modalBtnsOpen.forEach((modalBtnOpen) => {
+    modalBtnOpen.addEventListener('click', toggleModal);
+})
 
-modalBtnOpen.addEventListener('click', toggleModal);
-modalBtnOpen2.addEventListener('click', toggleModal);
+// modalBtnOpen2.addEventListener('click', toggleModal);
 modalBtnClose.addEventListener('click', toggleModal);
